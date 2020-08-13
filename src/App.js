3825decdb210cfb3741a7ad2aefd646561aee35b
Sanketmundada <mundadasanket2000@gmail.com>
2020-08-13 13:45:15 +0530

    Paint App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import randomColor from "randomcolor";
+import "./App.css";
+import Name from "./components/Name";
+import { ColorPicker } from "./components/ColorPicker";
+import { WindowSize } from "./components/WindowSize";
+import Canvas from "./components/Canvas";
+import RefreshButton from "./components/RefreshButton";
 
 function App() {
+  const [colors, setColors] = useState([]);
+  const [activeColor, setActiveColor] = useState(null);
+
+  const getColor = useCallback(() => {
+    const baseColor = randomColor().slice(1);
+    fetch(`https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome`)
+      .then((res) => res.json())
+      .then((res) => {
+        setColors(res.colors.map((color) => color.hex.value));
+        setActiveColor(res.colors[0].hex.value);
+      });
+  }, []);
+  useEffect(getColor, []);
+
+  const headerRef = useRef({ offsetHeight: 0 });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+        ref={headerRef}
+        style={{ borderTop: `10px solid ${activeColor}` }}
+      >
+        <div>
+          <Name />
+        </div>
+        <div style={{ marginTop: 10 }}>
+          <ColorPicker
+            colors={colors}
+            activeColor={activeColor}
+            setActiveColor={setActiveColor}
+          />
+          <RefreshButton cb={getColor} />
+        </div>
       </header>
+      {activeColor && (
+        <Canvas
+          color={activeColor}
+          height={window.innerHeight - headerRef.current.offsetHeight}
+        />
+      )}
+      <WindowSize />
     </div>
   );
 }
---
+++ src/components/WindowSize.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+export const WindowSize = () => {
+  const [[windowWidth, windowHeight], setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    let timeoutId;
+    const handleResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+      setVisible(true);
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => setVisible(false), 500);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  return (
+    <div className={`window-size ${visible ? "" : "hidden"}`}>
+      {windowWidth} x {windowHeight}
+    </div>
+  );
+};
